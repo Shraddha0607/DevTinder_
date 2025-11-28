e1049ab8aa808b004a1d9a3a3daecb189a29f72c
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-11-28 23:19:55 +0530

    write middlewares

--- devTinderBackend/homework.md
+++ devTinderBackend/homework.md
@@ -22,3 +22,13 @@
 - use of reges in routes /a/, /.*fly$/
 - reading the query params in the routes
 - reading the dynamic routes
+
+- multiple route handlers - play with the code
+- next()
+- next function and errors along with res.send()
+- app.use("/route", rH, [rH2, rH3, rH4], rH5, rH6);
+- what is middleware? Why do we need it?
+- how express JS basically handles requests behind the scene
+- difference app.use and app.all
+- write a dummy auth middleware for admin
+- write a dummy auth middleware for all user routes, except /user/login
--- devTinderBackend/src/app.js
+++ devTinderBackend/src/app.js
@@ -2,31 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:Id/:name", (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-    res.send({
-        firstName: "Shraddha",
-        lastName: "Gaur"
-    });
+const {
+    adminAuth,
+    userAuth
+} = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+app.use ("/user", userAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("I am user");
 });
 
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data successfully saved to the db");
+app.delete("/admin/deleteAllData", (req, res, next) => {
+    res.send("deletion by admin");
 });
 
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully!");
+app.delete("/admin/user", (req, res, next) => {
+    const token = "xyz";
+    if (token === "xyz") {
+        console.log("valid token");
+        res.send("User deleted successfully!");
+    } else {
+        res.status(401).send("Unauthorized request");
+    }
 });
 
-app.use("/test", (req, res) => {
-    res.send("Hello, from tester")
-});
-
-// app.use("/", (req, res) => {
-//     res.send("Namaste ji");
-// });
+app.get("/user", (req, res, next) => {
+        console.log("Handling the route user!!");
+        res.send("Response!!");
+    }
+);
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 2000");
