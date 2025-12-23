1d4511167ffc20df1604d65c4a2ffe28eb44248e
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-12-23 15:29:58 +0530

    feed API + pagination done

--- devTinderBackend/homework.md
+++ devTinderBackend/homework.md
@@ -124,3 +124,19 @@ validate, data, sanitize data, attacks should not aalow random data
 - Read about ref and populate https://mongoosejs.com/docs/populate.html
 - Create API /user/requests/received
 - Create GET /user/connection
+
+- Logic for GET /feed API
+- Explore the $nin, $and, $ne  and other "query operator(comparison query) "
+- Pagination
+
+NOTES:
+
+/feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)
+
+/feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)
+
+/feed?page=3&limit=10 => 20-30 => .skip(20) & .limit(10)
+
+/feed?page=4&limit=10 => 30-40 => .skip(30) & .limit(10)
+
+skip = (page-1) * limit;
--- devTinderBackend/src/routes/user.js
+++ devTinderBackend/src/routes/user.js
@@ -4,6 +4,7 @@ const {
     userAuth
 } = require('../middlewares/auth');
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -92,4 +93,72 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 
 });
 
+// what to show in card  ?page=1&limit=10
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+
+        const skip = (page - 1) * limit;
+
+        const connectionRequests = await ConnectionRequest.find({
+                $or: [{
+                        toUserId: loggedInUser._id
+                    },
+                    {
+                        fromUserId: loggedInUser._id
+                    }
+                ]
+            }).select("fromUserId toUserId")
+            .populate("fromUserId", "firstName")
+            .populate("toUserId", "firstName");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId);
+            hideUsersFromFeed.add(req.toUserId);
+        });
+
+        const users = await User.find({
+                $and: [{
+                        _id: {
+                            $nin: Array.from(hideUsersFromFeed)
+                        }
+                    }, // $nin: not in
+                    {
+                        _id: {
+                            $ne: loggedInUser._id
+                        }
+                    }, // $ne = not equal
+                ]
+            }).select(USER_SAFE_DATA)
+            .skip(skip).limit(limit);
+
+        if (users.length === 0) {
+            return res.status(400).json({
+                message: "No more feed available. Try after some time.",
+            });
+        }
+
+        return res.status(200).json({
+            message: "All feed data fetched successfully.",
+            data: users
+        });
+
+
+        // first check loggedInUser or not
+        // get all the user
+        // filter out all those 
+        // 1) not show user all rejected, accepted, interested, ignored
+        // 2) not show self one
+        // 3) 
+    } catch (err) {
+        return res.status(400).json({
+            message: "ERROR : " + err.message
+        });
+    }
+});
 module.exports = userRouter;
