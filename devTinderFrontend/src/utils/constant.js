ae8fd5192b0af88b92021c30142b6d1dde71c670
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-12-26 16:51:29 +0530

    git VITE_ENV used

--- devTinderFrontend/src/utils/constant.js
+++ devTinderFrontend/src/utils/constant.js
@@ -1,6 +1,2 @@
-// production (deployed)
-export const BASE_URL = "https://dev-tinder-beryl-gamma.vercel.app";
-
-
-// dev
-// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+    import.meta.env.VITE_API_BASE_URL;
