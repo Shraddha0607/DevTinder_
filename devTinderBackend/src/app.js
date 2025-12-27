82d5bfd1260112b8722c41135dc55f774868c6a8
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-12-27 11:12:15 +0530

    added options in CORS

--- devTinderBackend/src/app.js
+++ devTinderBackend/src/app.js
@@ -19,13 +19,13 @@ const cors = require("cors");
 const app = express();
 
 const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? [process.env.FRONTEND_URL]
-    : ["http://localhost:5173"];
+    process.env.NODE_ENV === "production" ? [process.env.FRONTEND_URL] : ["http://localhost:5173"];
 
 app.use(cors({
     origin: allowedOrigins,
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(cookieParser());
